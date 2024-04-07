88405a78f45e8b5277fdfc490054c42175bcc038
noelmagondim <noelmagondim@gmail.com>
2024-04-07 14:26:25 -0300

    Implementando a função principal do exercício 04

--- src/main/kotlin/Req04.kt
+++ src/main/kotlin/Req04.kt
@@ -15,5 +15,28 @@ fun calculateDivision(number1: Double, number2: Double): Double? {
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    println("Digite o primeiro número: ")
+    val number1 = readln().toDouble()
+
+    println("Digite o segundo número: ")
+    val number2 = readln().toDouble()
+
+    println("Escolha o número da função que deseja implementar:")
+    println("1 - Média entre os números digitados" +
+            "2 - Diferença do maior pelo menor" +
+            "3 - Produto entre os números digitados" +
+            "4 - Divisão do primeiro pelo segundo")
+    val choise = readln().toDouble()
+
+    val average = calculateAverage(number1, number2)
+    val difference = calculateDifference(number1, number2)
+    val product = calculateProduct(number1, number2)
+    val division = calculateDivision(number1, number2)
+
+    when (choise) {
+        1.0 -> println("A média é $average")
+        2.0 -> println("A diferença é $difference")
+        3.0 -> println("O produto é $product")
+        4.0 -> println("A divisão é $division")
+    }
 }
