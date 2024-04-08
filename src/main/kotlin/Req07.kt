24568908ccdf9b5cd9b3aaec9b696225014091ae
noelmagondim <noelmagondim@gmail.com>
2024-04-08 13:35:02 -0300

    implementando clear code no req 07

--- src/main/kotlin/Req07.kt
+++ src/main/kotlin/Req07.kt
@@ -32,16 +32,23 @@ fun calculatePercentageAgeHeight(ages: List<Int>, heights: List<Double>): Int {
 }
 
 fun main() {
-    val ages = listOf(25, 30, 35, 40, 45)
+    println("Digite a idade de 5 pessoas separadas por espaços: ")
+    val input = readln()
+    val ages = input.split(" ").map { it.toInt() }
     val average = calculateAverage(ages)
     println("Média das idades 25, 30, 35, 40, 45: $average")
 
-    val weights = listOf(80.0, 95.0, 70.0, 100.0, 98.0)
-    val heights = listOf(1.95, 1.96, 1.60, 1.96, 1.45)
+    println("Digite o peso de 5 pessoas separadas por espaços: ")
+    val inputWeights = readln()
+    val weights = inputWeights.split(" ").map { it.toDouble() }
+
+    println("Digite a altura de 5 pessoas separadas por espaços: ")
+    val inputHeights = readln()
+    val heights = inputHeights.split(" ").map { it.toDouble() }
+
     val count = countPeopleWeightHeight(weights, heights)
     println("A quantidade de pessoas com peso superior a 90kg e altura inferior a 1,50m é: $count")
 
     val percente = calculatePercentageAgeHeight(ages,heights)
     println("A porcentagem de pessoas com idade entre 10 e 30 anos que medem mais de 1,90m: $percente")
-
 }
