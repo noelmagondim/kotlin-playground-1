126ee2f508179e6df9ac59589a31522ec33e1c08
noelmagondim <noelmagondim@gmail.com>
2024-04-08 19:58:07 -0300

    Implementando main do req 08

--- src/main/kotlin/Req08.kt
+++ src/main/kotlin/Req08.kt
@@ -7,5 +7,11 @@ fun calculateSumOdd(numbers: List<Int>): Int {
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    println("Informe 10 números separados por um espaço: ")
+    val inputNumbers= readln()
+    val numbers = inputNumbers.split(" ").map { it.toInt() }
+    val sumEven = calculateSumEven(numbers)
+    val sumOdd = calculateSumOdd(numbers)
+    println("A soma dos números pares é: $sumEven")
+    println("A soma dos números impares é: $sumOdd")
 }
