0121e7444691499383dea0b02fe6dbe975e4c855
noelmagondim <noelmagondim@gmail.com>
2024-04-08 21:10:57 -0300

    Implementando mains do req 09

--- src/main/kotlin/Req09.kt
+++ src/main/kotlin/Req09.kt
@@ -3,5 +3,23 @@ fun calculateFinalPrice(carValue: Double, installment: Int, surcharges: Array<Do
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    println("Informe o valor do veículo: ")
+    val carValue = readln().toDouble()
+
+    println("Digite o número referente a quantidade de parcelas: ")
+    println("1 - 20% de desconto")
+    println("12 - 6% de acréscimo")
+    println("24 - 12% de acréscimo")
+    println("36 - 18% de acréscimo")
+    println("48 - 24% de acréscimo")
+    println("60 - 30% de acréscimo")
+    val installment = readln().toInt()
+    val surcharges = arrayOf(0.06, 0.12, 0.18, 0.24, 0.30)
+
+    val finalValue = calculateFinalPrice(carValue, installment, surcharges)
+    val installmentPrice = finalValue / installment
+
+    println("O valor da parcela é de: $installmentPrice")
+    println("O preço final é de: $finalValue")
+
 }
