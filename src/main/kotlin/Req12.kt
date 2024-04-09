9969d422af89538a3e782945d894e9391ee4599a
noelmagondim <noelmagondim@gmail.com>
2024-04-09 12:37:22 -0300

    Req 12

--- src/main/kotlin/Req12.kt
+++ src/main/kotlin/Req12.kt
@@ -1,7 +1,18 @@
 fun multiplyLists(list1: List<Int>, list2: List<Int>, result: MutableList<Int>) {
-    TODO("Use essa função para desenvolver sua lógica")
+    for (i in list1.indices) {
+        result.add(list1[i] * list2[i])
+    }
 }
 
 fun main() {
-    TODO("Use essa função para rodar o codigo principal")
+    println("Informe 10 números separados por espaço: ")
+    val list1 = readln().split(" ").map { it.toInt() }
+
+    println("Informe outros 10 números separados por espaço: ")
+    val list2 = readln().split(" ").map { it.toInt() }
+
+    val result: MutableList<Int> = mutableListOf()
+    multiplyLists(list1, list2, result)
+
+    println("O resultado da multiplicação de cada índice das listas é $result")
 }
